0b39eacb91621b59e5f3ab9445b785d64a0bb415
Haru <harumolodec@mail.ru>
2022-08-15 12:34:44 +0700

    Zadacha1 END

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int numbers = 5;
+Console.Write("Введите " + numbers + " чисел через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Чисел больше 0: {count}");
+
+
+
